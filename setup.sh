0e27141be5a380d476513127c66eb39c1d7e8c22
Nick Anthony <nanthony007@gmail.com>
2020-11-12 19:12:30 -0500

    Manually write setup.sh for heroku

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
 mkdir -p ~/.streamlit/
-echo “\
-[general]\n\
-email = \”your-email@domain.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+\n\
+" > ~/.streamlit/config.toml
